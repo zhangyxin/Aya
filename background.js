0e100df6f99d5720433b33122226992c8f928a95
zhangyuxin <zyxwy1206@163.com>
2023-03-27 17:07:11 +0800

    add chrome.extension.onRequest

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("The color is green.");
+      console.log("set color is green.");
     });
 
     //
@@ -13,4 +13,13 @@ chrome.runtime.onInstalled.addListener(function() {
             actions: [new chrome.declarativeContent.ShowPageAction()]
       }]);
     });
-  });
+});
+  
+// 注册回调，当收到请求的时候触发
+chrome.extension.onRequest.addListener(({ tabId, args }) => {
+    console.log("Register chrome.extension.onRequest");
+    // 在给定tabId的tab页中执行脚本
+    chrome.tabs.executeScript(tabId, {
+        code: `console.log(...${JSON.stringify(args)});`,
+    });
+});
